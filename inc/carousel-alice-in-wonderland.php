56592bf54edf9ebb2ba221a95fc3286b863336d4
stevaras <steve_stizzy@yahoo.com>
2021-03-23 17:14:32 +0200

    Update images for the latest 4 new rooms

--- inc/carousel-alice-in-wonderland.php
+++ inc/carousel-alice-in-wonderland.php
@@ -26,6 +26,9 @@
         <div class="carousel-item">
             <img class='w-100' src="/img/room_slider_images/alice_in_wonderland/8.jpg" alt="Alice In Wonderland Image">
         </div>
+        <div class="carousel-item">
+            <img class='w-100' src="/img/room_slider_images/alice_in_wonderland/9.jpg" alt="Alice In Wonderland Image">
+        </div>
     </div> <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only">Previous</span> </a> <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only">Next</span> </a>
     <section id="section10" class="demo"> <a class="js-scroll-trigger" href="#mainsect">
             <div class="text-center arrow-button" style="">
